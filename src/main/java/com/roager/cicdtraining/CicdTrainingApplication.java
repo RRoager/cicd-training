9a70035f8ee2aa75bb691c3f7da393ad4e089935
Rasmus Roager <rasmus-roager@hotmail.com>
2023-11-09 15:39:54 +0100

    first commit

---
+++ src/main/java/com/roager/cicdtraining/CicdTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.roager.cicdtraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roager/cicdtraining/CicdTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roager.cicdtraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
